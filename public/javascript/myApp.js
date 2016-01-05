6510035e1cba427c27f273e591bbf68ee615c280
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-05 22:36:06 +0000

    map displaying on page

--- public/javascript/myApp.js
+++ public/javascript/myApp.js
@@ -5,4 +5,21 @@ myApp.controller('AppCtrl', [
   '$http',
   function($scope, $http) {
     console.log("hello world from controller");
+
+    var startmap = function() {
+
+      var map;
+      function initialize() {
+        var center = new google.maps.LatLng(37.422, -122.084058);
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: center,
+          zoom: 13
+
+        });
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize);
+    }
+
+    startmap();
   }]);
