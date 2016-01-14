e9c3527de06657c691a50c4a755bafac4e0994b9
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-14 20:41:47 +0000

    bit of refactoring

--- public/javascript/myApp.js
+++ public/javascript/myApp.js
@@ -6,20 +6,33 @@ myApp.controller('AppCtrl', [
   function($scope, $http) {
     console.log("hello world from controller");
 
-    var startmap = function() {
+    // var startmap = function() {
+    //
+    //   var map;
+    //   function initialize() {
+    //     var center = new google.maps.LatLng(37.422, -122.084058);
+    //     map = new google.maps.Map(document.getElementById('map'), {
+    //       center: center,
+    //       zoom: 13
+    //
+    //     });
+    //   }
+    //
+    //   google.maps.event.addDomListener(window, 'load', initialize);
+    // }
+    //
+    // startmap();
 
-      var map;
-      function initialize() {
-        var center = new google.maps.LatLng(37.422, -122.084058);
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: center,
-          zoom: 13
+    function initialize() {
+   var mapCanvas = document.getElementById('map');
+   var mapOptions = {
+     center: new google.maps.LatLng(44.5403, -78.5463),
+     zoom: 8,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+   }
+   var map = new google.maps.Map(mapCanvas, mapOptions);
+ }
 
-        });
-      }
+ google.maps.event.addDomListener(window, 'load', initialize);
 
-      google.maps.event.addDomListener(window, 'load', initialize);
-    }
-
-    startmap();
   }]);
