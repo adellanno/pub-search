a872e774cf7f4e96aa6163c416c940b462786f3a
Anthony Dell'Anno <anthony@dellanno.co.uk>
2016-01-14 22:16:26 +0000

    some pubs now showing up in results

--- public/javascript/myApp.js
+++ public/javascript/myApp.js
@@ -6,33 +6,60 @@ myApp.controller('AppCtrl', [
   function($scope, $http) {
     console.log("hello world from controller");
 
-    // var startmap = function() {
-    //
-    //   var map;
-    //   function initialize() {
-    //     var center = new google.maps.LatLng(37.422, -122.084058);
-    //     map = new google.maps.Map(document.getElementById('map'), {
-    //       center: center,
-    //       zoom: 13
-    //
-    //     });
-    //   }
-    //
-    //   google.maps.event.addDomListener(window, 'load', initialize);
+
+    var map;
+    var service;
+    var infowindow;
+
+  function initialize() {
+
+    var currentLocation = new google.maps.LatLng(51.4290249,-0.3617751); // current location set as variable
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: currentLocation,
+      zoom: 10
+    });
+
+    var request = {
+      location: currentLocation,
+      radius: '1000',
+      query: 'pub near tw12 1hh'
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+
+    function createMarker(place){
+        var photos = place.photos;
+        if (!photos) {
+          return;
+        }
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          title: place.name,
+          icon: photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35})
+        });
+    }
+
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          createMarker(results[i]);
+        }
+      }
+    }
+    // var mapCanvas = document.getElementById('map');
+    // var mapOptions = {
+    //  center: new google.maps.LatLng(44.5403, -78.5463),
+    //  zoom: 8,
+    //  mapTypeId: google.maps.MapTypeId.ROADMAP
     // }
-    //
-    // startmap();
+    // var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
 
-    function initialize() {
-   var mapCanvas = document.getElementById('map');
-   var mapOptions = {
-     center: new google.maps.LatLng(44.5403, -78.5463),
-     zoom: 8,
-     mapTypeId: google.maps.MapTypeId.ROADMAP
-   }
-   var map = new google.maps.Map(mapCanvas, mapOptions);
- }
-
- google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
 
   }]);
